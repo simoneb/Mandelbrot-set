704c3c3c855112b461de95b7a884e2f697672e1a
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2020-11-06 15:57:14 +0000

    added a nicer interface for Workers

--- src/generate.ts
+++ src/generate.ts
@@ -1,5 +1,29 @@
-export function generate(){
-    const worker = new Worker("build/worker.js");
-    worker.onmessage = event => console.log(event.data);
-    worker.postMessage(100);
+class Thread {
+    static wasm: ArrayBuffer;
+    worker: Worker;
+    constructor() {
+        this.worker = new Worker("build/worker.js");
+    }
+    async sendWasm() {
+        this.command("sendWasm", await Thread.getWasm());
+    }
+    command(action: string, data) {
+        return new Promise((resolve, reject) => {
+            this.worker.onmessage = event => resolve(event.data);
+            this.worker.postMessage({ action, data });
+        });
+    }
+    static async getWasm() {
+        if (typeof Thread.wasm === "undefined") {
+            const response = await fetch("build/generate.wasm");
+            Thread.wasm = await response.arrayBuffer();
+        }
+        return Thread.wasm;
+    }
+}
+
+export async function generate() {
+    const thread = new Thread();
+    //await thread.sendWasm();
+    console.log(await thread.command("plus1", 100));
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,19 @@
+import loader from "@assemblyscript/loader";
+
+interface command {
+    action: string,
+    data: any
+}
+
 const myself: Worker = self as any;
 myself.onmessage = (event) => {
-    myself.postMessage(event.data + 2);
+    const { action, data } = event.data as command;
+    switch (action) {
+        case "loadWasm":
+            loader.instantiate(data);
+            break;
+        case "plus1":
+            myself.postMessage(data+1);
+            break;
+    }
 }
