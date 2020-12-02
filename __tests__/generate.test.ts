f1f7ff24b21115291660b8f4a31849683b3b3432
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2020-12-02 21:27:10 +0000

    added 2 more tests for generate.ts

--- __tests__/generate.test.ts
+++ __tests__/generate.test.ts
@@ -1,10 +1,29 @@
 import { generate, Thread } from "../src/generate";
 import { Point } from "../src/point";
 
-describe("test", () => {
-    test("a", async () => {
-        expect(await generate(1, 1, 0.5, new Point(0, 0), false, 50, 2))
+describe("test the output of the generate function", () => {
+    test("at point (0, 0) without color", async () => {
+        expect(await generate(1, 1, 0.5, new Point(0, 0), false, 50, 1))
             .toStrictEqual(new Uint8ClampedArray([255, 255, 255, 255]));
+    });
+
+    test("around (0, 0) with color", async () => {
+        expect(await generate(2, 2, 0.5, new Point(0, 0), true, 50, 2))
+            .toStrictEqual(new Uint8ClampedArray([
+                255, 25, 0, 255,
+                0, 0, 0, 255,
+                0, 0, 0, 255,
+                0, 0, 0, 255
+            ]));
+    });
+
+    test("at point (0.4, -0.3) with color", async () => {
+        expect(await generate(1, 1, 20, new Point(0.4, -0.3), true, 50, 1))
+            .toStrictEqual(new Uint8ClampedArray([0, 229, 255, 255]));
+    })
+
+    afterEach(() => {
         Thread.changeNumberOfThreads(0);
+        // terminate all threads
     })
 });
