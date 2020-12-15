31ba5c0728a066c387669a2d63c762a03441b960
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2020-12-15 19:59:06 +0000

    installed GPU.js and created gpu-generate.ts

---
+++ src/gpu-generate.ts
@@ -0,0 +1,13 @@
+import { GPU } from "gpu.js";
+import type { Point } from "./point";
+
+export function gpuGenerate(
+    width: number,
+    height: number,
+    zoom: number,
+    offset: Point,
+    color: boolean,
+    numberOfIterations: number
+) {
+    
+}
