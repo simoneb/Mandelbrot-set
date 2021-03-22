bf1b8e76aa959b77f07b7112e0e4c44e4267d490
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2021-03-22 16:00:09 +0000

    updated the jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/assembly/']
+    testPathIgnorePatterns: ['/node_modules/', '/assembly/'],
+    transform: {
+        "^.+\\.svelte$": [
+            "svelte-jester",
+            { preprocess: true }
+        ],
+        "^.+\\.ts$": "ts-jest"
+    },
+    moduleFileExtensions: [
+        "js",
+        "ts",
+        "svelte"
+    ]
 };
